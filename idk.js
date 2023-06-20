e85b88b3700facb82fec5b59ade5111fea1b2e46
c00lman <137161436+c00lman-offic@users.noreply.github.com>
2023-06-20 20:26:26 +0300

    Delete idk.js

--- idk.js
+++
@@ -1,3 +0,0 @@
-<a href="https://scriptunc.org" style="height:max-content;" target="_blank">
-    <img style="width: 120px;height:auto;" alt="UNC Supported Badge" src="https://scriptunc.org/badge-supported.png"/>
-</a>
